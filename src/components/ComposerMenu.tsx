81f04b42730d167a2e4f0d3fb3d3ae8970c5e7c3
Erica Burgos <ericab79@gmail.com>
2025-05-06 09:30:17 +0700

    Updated ComposerMenu padding and corrected image URLs for composers, added name for Frédéric Chopin.

--- src/components/ComposerMenu.tsx
+++ src/components/ComposerMenu.tsx
@@ -62,7 +62,7 @@ export function ComposerMenu({
 
       <Timeline selectedEra={selectedEra} onSelectEra={handleEraChange} />
 
-      <div className="px-2 md:px-6 pb-8 flex-grow overflow-y-auto">
+      <div className="px-2 md:px-6 pb-0 flex-grow overflow-y-auto">
         <ComposerList
           era={selectedEra}
           onSelectComposer={onSelectComposer}
